7a43a350d23bc8c4e9fbfac1046e7d30ec41f89d
Ohno17 <ajaylego30@gmail.com>
2025-07-26 13:50:34 -0500

    even more minor change../ why did i change this..f; k;ef'opPO@{I

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {4, -5, -6},     // Left Chassis Ports (negative port will reverse it!)
-    {-1, 2, 3},  // Right Chassis Ports (negative port will reverse it!)
+    {4, -5, -6},    // Left Chassis Ports (negative port will reverse it!)
+    {-1, 2, 3},     // Right Chassis Ports (negative port will reverse it!)
 
     7,      // IMU Port
     4.125,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
